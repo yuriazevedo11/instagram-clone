0043bdb1d27140d0d8397c2717e440536e5a7dcb
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-06-14 01:20:21 -0300

    New - Redimensionamento das imagens do post

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,4 +1,8 @@
 const Post = require('../models/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
+
 
 module.exports = {
   async index(req, res) {
@@ -10,12 +14,24 @@ module.exports = {
     const { author, place, description, hashtags } = req.body
     const { filename: image } = req.file
 
+    const [name] = image.split(/\.(?=[^\.]+$)/)
+    const fileName = `${name}.jpg`
+
+    await sharp(req.file.path)
+      .jpeg({ quality: 70 })
+      .resize(500)
+      .toFile(
+        path.resolve(req.file.destination, 'resized', fileName)
+      )
+
+    fs.unlinkSync(req.file.path)
+
     const post = await Post.create({
       author,
       place,
       description,
       hashtags,
-      image
+      image: fileName
     })
 
     return res.json(post)

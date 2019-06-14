a5320860275a6f61ca64b3b6628c59266efe4109
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-06-14 00:02:59 -0300

    New - Rota para criação de um post e suas respectivas configurações

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
+require('dotenv/config');
 const express  = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect('urlToDB', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true
 })
+
+app.use(require('./routes'))
 
 app.listen(3000)

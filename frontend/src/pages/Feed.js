f91ade1801220e46eb4ef2508c070fc5a278a0e1
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-06-15 16:47:22 -0300

    New - Informações do seed carregadas pelo backend

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import api from '../services/api'
 
 import './Feed.css'
 
@@ -8,36 +9,48 @@ import comment from '../assets/comment.svg'
 import send from '../assets/send.svg'
 
 export default class Feed extends Component {
+  state = {
+    feed: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('posts')
+
+    this.setState({ feed: response.data })
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Yuri Azevedo</span>
-              <span className="place">Rio de Janeiro</span>
-            </div>
+        {this.state.feed.map(post => (
+          <article>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-            <img src={more} alt="Mais" />
-          </header>
+              <img src={more} alt="Mais" />
+            </header>
 
-          <img src="http://localhost:3001/files/kiara-brasil.jpg" alt="" />
+            <img src={`http://localhost:3001/files/${post.image}`} alt="" />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="" />
-              <img src={comment} alt="" />
-              <img src={send} alt="" />
-            </div>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="" />
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
 
-            <strong>31 curtidas</strong>
+              <strong>{post.likes} curtidas</strong>
 
-            <p>
-              Um post qualquer
-              <span>#teste #opa</span>
-            </p>
-          </footer>
-        </article>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     )
   }
